0859f21b59dc8ebcaec4fa3f4421302ea98cfddc
Chaitanya <nalamchaitanya@gmail.com>
2019-12-05 23:13:24 -0500

    Added command line tool interface

--- Main.cpp
+++ Main.cpp
@@ -7,16 +7,34 @@ using namespace std;
 int main()
 {
 	FibHeap<int,string>* fib = new FibHeap<int,string>();
-	string name("abc");
-	int key = 4;
-	FibNode<int,string>* node = new FibNode<int,string>(key, name);
-	cout << *node << endl;
-	fib->Insert(node);
-	// FibHeap<int,int>* fib = new FibHeap<int,int>();
-	// string name("abc");
-	// int key = 4;
-	// FibNode<int,int>* node = new FibNode<int,int>(key, 5);
-	// cout << *node << endl;
-	// fib->Insert(node);
+	string name;
+	int key = 0;
+	while(true)
+	{
+		int t;
+		cout << "Type command: "
+		cin >> t;
+		switch(t)
+		{
+			case -1:
+				cout << "Bye bye" << endl;
+				return 0;
+			case 1:
+				cout << "Type Key: ";
+				cin >> key;
+				cout << "Type Data: ";
+				cin >> name;
+				fib->Insert(new FibNode<int,string>(key, name));
+				break;
+			case 2:
+				cout << *(fib->Minimum()) << endl;
+				break;
+			case 3:
+				cout << *(fib->ExtractMin()) << endl;
+				break;
+			case 4:
+				cout << *fib << endl;
+		}
+	}
 	return 0;
 }
