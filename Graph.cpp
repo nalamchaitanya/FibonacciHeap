6334f510b8a9b23cdcecaf0be20a0588f2d28a68
Chaitanya <nalamchaitanya@gmail.com>
2019-12-10 18:02:16 -0500

    Added Graph and BinHeap.cpp's

---
+++ Graph.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+vector<vector<std::pair<int,int> > > getGraph(string fileName)
+{
+	ifstream fin(fileName, std::ifstream::in);
+	int lines;
+	fin >> lines;
+	vector<vector<std::pair<int,int> > > adj(lines);
+	for(int i = 0;i<lines;i++)
+	{
+		int label, degree;
+		fin >> label >> degree;
+		int weight;
+		vector<pair<int,int> > list(degree);
+		for(int j = 0;j<degree;j++)
+		{
+			fin >> label >> weight;
+			list[j] = make_pair(label, weight);
+		}
+		adj[i] = list;
+	}
+	return adj;
+}
+
+void printGraph(vector<vector<std::pair<int,int> > > adj)
+{
+	for (int i = 0; i < adj.size(); i++) {
+	cout << "--------- " << i << endl;
+	vector<pair<int,int> > temp = adj[i];
+		for (int j = 0; j < temp.size(); j++) {
+			cout << temp[j].first << ", " << temp[j].second << endl;
+		}
+	}
+	cout << "--------- " << endl;
+}
