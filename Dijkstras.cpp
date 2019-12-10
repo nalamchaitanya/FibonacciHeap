fc08ebd44a86a0c012e9ea9915a7fb017c7e7211
Chaitanya <nalamchaitanya@gmail.com>
2019-12-10 18:19:52 -0500

    Dijkstras code complete

--- Dijkstras.cpp
+++ Dijkstras.cpp
@@ -7,22 +7,24 @@
 #include "BinHeap.h"
 #include "Djisktra.h"
 #include "Prims.h"
+#include "Graph.cpp"
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	if(argc != 2)
+	if(argc != 3)
 	{
 		cout << "Give two arguments" << endl;
 		return 0;
 	}
-	vector<vector<std::pair<int,int> > > adj = getGraph(argv[0]);
-	int heapType = stoi(argv[1]);
+	vector<vector<std::pair<int,int> > > adj = getGraph(argv[1]);
+	int heapType = stoi(argv[2]);
 	Djisktra* djisktra = new Djisktra();
 	pair<vector<int>, unordered_map<int, int> > output;
 	if(heapType == 0)
-		output = djisktra->runDjisktraWithBin(adj, sourceNode);
+		output = djisktra->runDjisktraWithBin(adj, 0);
 	else
-		output = djisktra->runDjisktraWithFib(adj, sourceNode);
+		output = djisktra->runDjisktraWithFib(adj, 0);
+	printResult(output);
 	return 0;
 }
