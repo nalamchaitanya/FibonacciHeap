99cf21fa89c84718a64a539c73bff590df14f1dd
Chaitanya <nalamchaitanya@gmail.com>
2019-12-10 18:00:08 -0500

    Copied runDijkstras with Fib Heap

--- Djisktra.h
+++ Djisktra.h
@@ -19,7 +19,7 @@ public:
 	
 	}
 
-	std::pair<vector<int>, unordered_map<int, int> > runDjisktra(vector<vector<std::pair<int, int> > > adj, int sourceNode) {
+	std::pair<vector<int>, unordered_map<int, int> > runDjisktraWithBin(vector<vector<std::pair<int, int> > > adj, int sourceNode) {
 		int size = adj.size();
 		unordered_map<int, int> nodeToParentMapping(size);
 		vector<int> distances(size);
@@ -30,23 +30,63 @@ public:
 
 		distances[sourceNode] = 0;
 
-		// BinNode<int, int>* node = new BinNode<int, int>(0, sourceNode);
-		FibNode<int, int>* node = new FibNode<int, int>(0, sourceNode);
-		// BinHeap<int, int>* heap = new BinHeap<int, int>();
-		FibHeap<int, int>* heap = new FibHeap<int, int>();
+		BinNode<int, int>* node = new BinNode<int, int>(0, sourceNode);
+		BinHeap<int, int>* heap = new BinHeap<int, int>();
 		heap->Insert(node);
 
-		while (!heap->IsEmpty()) {
+		while (!heap->IsEmpty())
+		{
 			node = heap->ExtractMin();
 			int currentNodeIndex = node->data;
 			delete node;
 			vector<std::pair<int, int> > neighbors = adj[currentNodeIndex];
-			for (int i = 0; i < neighbors.size(); i++) {
-				if (distances[neighbors[i].first] == INT_MAX) {
-					// heap->Insert(new BinNode<int, int>(INT_MAX, neighbors[i].first));
+			for (int i = 0; i < neighbors.size(); i++)
+			{
+				if (distances[neighbors[i].first] == INT_MAX)
+				{
+					heap->Insert(new BinNode<int, int>(INT_MAX, neighbors[i].first));
+				}
+				if (distances[currentNodeIndex] + neighbors[i].second < distances[neighbors[i].first])
+				{
+					distances[neighbors[i].first] = distances[currentNodeIndex] + neighbors[i].second;
+					nodeToParentMapping[neighbors[i].first] = currentNodeIndex;
+					heap->DecreaseKey(neighbors[i].first, distances[neighbors[i].first]);
+				}
+			}
+		}
+		pair<vector<int>, unordered_map<int, int> > output = make_pair(distances, nodeToParentMapping);
+		return output;
+	}
+
+	std::pair<vector<int>, unordered_map<int, int> > runDjisktraWithFib(vector<vector<std::pair<int, int> > > adj, int sourceNode) {
+		int size = adj.size();
+		unordered_map<int, int> nodeToParentMapping(size);
+		vector<int> distances(size);
+
+		for (int i = 0; i < size; i++) {
+			distances[i] = INT_MAX;
+		}
+
+		distances[sourceNode] = 0;
+
+		FibNode<int, int>* node = new FibNode<int, int>(0, sourceNode);
+		FibHeap<int, int>* heap = new FibHeap<int, int>();
+		heap->Insert(node);
+
+		while (!heap->IsEmpty())
+		{
+			node = heap->ExtractMin();
+			int currentNodeIndex = node->data;
+			delete node;
+			vector<std::pair<int, int> > neighbors = adj[currentNodeIndex];
+			for (int i = 0; i < neighbors.size(); i++)
+			{
+				if (distances[neighbors[i].first] == INT_MAX)
+				{
 					heap->Insert(new FibNode<int, int>(INT_MAX, neighbors[i].first));
 				}
-				if (distances[currentNodeIndex] + neighbors[i].second < distances[neighbors[i].first]) {
+				if (distances[currentNodeIndex] + neighbors[i].second < distances[neighbors[i].first])
+				{
 					distances[neighbors[i].first] = distances[currentNodeIndex] + neighbors[i].second;
 					nodeToParentMapping[neighbors[i].first] = currentNodeIndex;
 					heap->DecreaseKey(neighbors[i].first, distances[neighbors[i].first]);
