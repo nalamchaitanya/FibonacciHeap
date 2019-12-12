fd4759f51a6dc66f757231030f80be7552195597
Chaitanya <nalamchaitanya@gmail.com>
2019-12-11 22:32:41 -0500

    Changed Compare.py

--- Comapre.py
+++ Comapre.py
@@ -12,24 +12,40 @@ def getResults():
 
 def plotGraph():
 	for algo in ["Dijkstras", "Prims"]:
-		vals = [range(9),range(9)]
+		vals = [[],[]]
 		for ds in [0,1]:
 			count = 0;
 			with open(algo+str(ds)+".res") as file:
 				for line in file:
-					vals[ds][count] = int(line)
-					count += 1
-		res = []
-		for i in range(9):
-			res.append((vals[0][i],vals[1][i]))
-		res.sort();
-		for i in range(9):
-			vals[0][i] = res[i][0]
-			vals[1][i] = res[i][1]
+					(edges,time) = line.split(" ")
+					vals[ds].append((int(edges),int(time)))
+		# res = []
+		# for i in range(9):
+		# 	res.append((vals[0][i],vals[1][i]))
+		# res.sort();
+		vals[0].sort()
+		vals[1].sort()
+
+		binlist = []
+		edges = []
+		fiblist = []
+		print(vals)
+		for i in range(len(vals[0])-1):
+			x,y = vals[0][i]
+			_, z = vals[1][i]
+			edges.append(x)
+			binlist.append(y)
+			fiblist.append(z)
+		print(edges)
+		print(binlist)
+		print(fiblist)
+		# for i in range(9):
+		# 	vals[0][i] = res[i][0]
+		# 	vals[1][i] = res[i][1]
 		fig = plt.figure()
-		plt.plot(range(9), vals[0], label="BinaryHeap")
-		plt.plot(range(9), vals[1], label="FibonacciHeap")
-		plt.xlabel('Graphs')
+		plt.plot(edges, binlist, label="BinaryHeap")
+		plt.plot(edges, fiblist, label="FibonacciHeap")
+		plt.xlabel('Number of Edges')
 		plt.ylabel('Time in microseconds')
 		plt.title(algo+': FibonacciHeap vs BinaryHeap')
 		plt.legend()
